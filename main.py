4d539020c89d28b9132ce1e108979b3537f647c6
Howard <howardyanghaowen@gmail.com>
2022-04-26 10:47:47 +0800

    normalise attr

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import numpy as np
 import torch
 
 import cv2
-from captum.attr import IntegratedGradients
+from captum.attr import (IntegratedGradients, GradientShap, 
+                         DeepLift, DeepLiftShap,
+                         Saliency, InputXGradient,
+                         Deconvolution, GuidedBackprop, GuidedGradCam,
+                         FeatureAblation, Occlusion)
 from detectron2 import model_zoo
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
@@ -77,6 +81,65 @@ def main():
     print('Integrated Gradients Convergence Delta:', delta)
     save_attr_mask(attributions, img, 'IG')
 
+    # Gradient SHAP
+    gs = GradientShap(wrapper)
+    # We define a distribution of baselines and draw `n_samples` from that
+    # distribution in order to estimate the expectations of gradients across all baselines
+    attributions, delta = gs.attribute(input_, stdevs=0.09, n_samples=4, baselines=baseline_dist,
+                                target=pred_class, return_convergence_delta=True)
+
+    print('GradientShap Convergence Delta:', delta)
+    print('GradientShap Average Delta per example:', torch.mean(delta.reshape(input_.shape[0], -1), dim=1))
+    save_attr_mask(attributions, img, 'GradientShap')
+
+    # Deep Lift
+    dl = DeepLift(wrapper)
+    attributions, delta = dl.attribute(input_, baseline, target=pred_class, return_convergence_delta=True)
+    print('DeepLift Convergence Delta:', delta)
+    save_attr_mask(attributions, img, 'DeepLift')
+
+    # DeepLiftShap
+    dls = DeepLiftShap(wrapper)
+    attributions, delta = dls.attribute(input_, baseline, target=pred_class, return_convergence_delta=True)
+    print('DeepLiftShap Convergence Delta:', delta)
+    print('Deep Lift SHAP Average delta per example:', torch.mean(delta.reshape(input_.shape[0], -1), dim=1))
+    save_attr_mask(attributions, img, 'DeepLiftShap')
+
+    # Saliency
+    saliency = Saliency(wrapper)
+    attribution = saliency.attribute(input_, target=pred_class)
+    save_attr_mask(attribution, img, 'Saliency')
+
+    # InputXGradient
+    inputxgradient = InputXGradient(wrapper)
+    attribution = inputxgradient.attribute(input_, target=pred_class)
+    save_attr_mask(attribution, img, 'InputXGradient')
+
+    # Deconvolution
+    deconv = Deconvolution(wrapper)
+    attribution = deconv.attribute(input_, target=pred_class)
+    save_attr_mask(attribution, img, 'Deconvolution')
+
+    # Guided Backprop
+    gbp = GuidedBackprop(wrapper)
+    attribution = gbp.attribute(input_, target=pred_class)
+    save_attr_mask(attribution, img, 'GuidedBackprop')
+
+    # # GuidedGradCam
+    # guided_gc = GuidedGradCam(wrapper, wrapper.model.backbone) # TODO: doesnt seem right
+    # attribution = guided_gc.attribute(input_, target=pred_class, attribute_to_layer_input=True)
+    # save_attr_mask(attribution, img, 'GuidedGradCam')
+
+    # FeatureAblation
+    ablator = FeatureAblation(wrapper)
+    attr = ablator.attribute(input_, target=pred_class, show_progress=True)
+    save_attr_mask(attr, img, 'FeatureAblation')
+
+    # Occlusion
+    ablator = Occlusion(wrapper)
+    attr = ablator.attribute(input_, target=pred_class, sliding_window_shapes=(1, 3,3))
+    save_attr_mask(attr, img, 'Occlusion')
+
 def new_preprocess_image(self, batched_inputs: torch.Tensor):
       """
       Normalize, pad and batch the input images.
@@ -86,9 +149,18 @@ def new_preprocess_image(self, batched_inputs: torch.Tensor):
       images = ModifiedImageList.from_tensors(images, self.backbone.size_divisibility) # Extend ImageList to new object
       return images
 
+
 def save_attr_mask(attributions, img, algo_name):
+    # C, H, W -> H, W, C
     attributions = attributions[0].permute(1,2,0).cpu().numpy()
+
+    # flattern to 1D
     attributions = np.sum(np.abs(attributions), axis=-1)
+
+    # normalise attributions
+    attributions -= np.min(attributions)
+    attributions /= np.max(attributions)
+
     _, axs = plt.subplots(nrows=1, ncols=2, squeeze=False, figsize=(8, 8))
     axs[0, 0].set_title('Attribution mask')
     axs[0, 0].imshow(attributions, cmap=plt.cm.inferno)
